144064d5bb17d74378cd59739d80219d83fc4440
osaaoui <saaoui@gmail.com>
2017-02-07 16:43:39 -0500

    ajouter les sliders

--- public_html/JS/clavierVirtuel.js
+++ public_html/JS/clavierVirtuel.js
@@ -2,7 +2,6 @@ $(function(){
     	$("#clavier-virtuel").find("button").click(function () {
     		//alert("Hi");
 		var text = $(this).text();
-		alert(text);
 		$("input").val($("input").val()+ text);
 		                
 });
