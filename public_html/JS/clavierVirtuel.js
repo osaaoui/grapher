732384d12e1a550ee9e57b2be4cbe15d04aedd6f
MARCOMP\Marco <olmocinese@hotmail.com>
2017-02-11 16:23:57 -0500

    changement dans le script clavierVirtuel.js - problemes regles changement de font

--- public_html/JS/clavierVirtuel.js
+++ public_html/JS/clavierVirtuel.js
@@ -1,9 +1,21 @@
 $(function(){
-    	$("#clavier-virtuel").find("button").click(function () {
-    		//alert("Hi");
-		var text = $(this).text();
-		$("input").val($("input").val()+ text);
-		                
-});
+  $("#clavier-virtuel").find("button").click(function () {
+    //alert("Hi");
+    var text = String.fromCharCode(178);
+    var text2;
+    var saisie = this.id;
+
+    if (saisie == "x2") {
+      text2 = "x";
+      text = text2.concat(text);
+    } else if (saisie == "y2") {
+      text2 = "y";
+      text = text2.concat(text);
+    }
+    var inputText= document.getElementById('input').value;
+
+    var res = inputText.concat(text);
+    document.getElementById("input").value = res;
+  });
 
 });
