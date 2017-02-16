6f90d69de3f2569295e08cf03a454402314dc883
osaaoui <saaoui@gmail.com>
2017-02-16 10:11:41 -0500

    modifier les fonctions qui retournent les parametres a et b

--- public_html/JS/indexScript.js
+++ public_html/JS/indexScript.js
@@ -14,30 +14,12 @@ function plot(func, atts){
 
 function trace(){
     var equation = document.getElementById('input').value;//alert(equation);
-    var tok = tokenize(equation);
-	var ordonnee;
-	var pente= /[0-9]+x/;
-	var sansX= /[0-9]+/;
-	var penteX= 'x';
-	for (var i=0; i< tok.length; i++){
-		if(tok[i].match(pente)){
-			//alert("parametre a= "+ tok[i]);
-			pente= tok[i].match(sansX);
-			
-			//alert("La pente est= " + pente);
-			
-		}else if (tok[i]==penteX){
-			pente= 1;
-			alert("La pente est= " + pente);
-		}else if(!isNaN(tok[i])){
-			ordonnee= tok[i];
-			//alert("ordonnee est: " + ordonnee);
-		}
-		
-	}
-	/*var tabPara=trouverPara(equation);
-	var paraA=tabPara[0][0]; //alert(paraA);
-	var paraB=tabPara[1][0]; //alert(paraB); */
+    var pente= parametreA(tokenize(equation));
+    alert("La pente = " + pente);
+    var ordonnee= parametreB(tokenize(equation));
+    alert("Ordonnee = " + ordonnee);
+    
+    
 	var sliderA =board.create('slider',[[4,-3],[6,-3],[pente-4,pente,pente+4]],{name:'a'});
 	var sliderB =board.create('slider',[[4,-3.5],[6,-3.5], [ordonnee -4, ordonnee,ordonnee +4]], {name:'b'});
 	function f(x) {
@@ -51,7 +33,7 @@ function clearAll(board){
 	return board;
 }
 
-function tokenize(code) {
+const tokenize= function (code) {
         var results = [];
         var tokenRegExp = /\s*([A-Za-z]+|[0-9]+x|\S)\s*/g;
 
@@ -59,7 +41,44 @@ function tokenize(code) {
         while ((m = tokenRegExp.exec(code)) !== null)
         results.push(m[1]);
         return results;
-   }     
+        
+};
+    const parametreA = function(code){
+	
+	//var equation = document.getElementById('input').value;//alert(equation);
+    var tok = tokenize(code);
+	var paraA;
+	var pente= /[0-9]+x/;
+	var sansX= /[0-9]+/;
+	var penteX= 'x';
+	for (var i=0; i< tok.length; i++){
+		if(tok[i].match(pente)){
+			//alert("parametre a= "+ tok[i]);
+			pente= tok[i].match(sansX);
+			paraA= pente;
+			//alert("La pente est= " + pente);
+			return paraA;
+		
+		}else if (tok[i]==penteX){
+			pente= 1;
+			paraA= pente;
+			return paraA;
+			//alert("La pente est= " + pente);
+	}
+	}
+};
+
+const parametreB = function(code){
+	var tok = tokenize(code);
+	var ordonnee;
+	for (var i=0; i< tok.length; i++){
+		 if(!isNaN(tok[i])){
+			ordonnee= tok[i];
+			return ordonnee;
+	}
+	}
+		
+};    
 
 /* function trouverPara(equation){
         equation=equation.replace(/ /g,"");
