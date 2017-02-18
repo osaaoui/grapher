b1b6c4bd140083c4fbf10d5a810ed22b756da8ed
etiennebl <etien@LAPTOP-GBL6F5F0>
2017-02-18 00:59:30 -0500

    Ajustement de la methode d'extraction des paramettres -les negatifs sont intégré -ainsi que plusieurs parametres

--- public_html/JS/indexScript.js
+++ public_html/JS/indexScript.js
@@ -34,77 +34,76 @@ function clearAll(board){
 }
 
 
-/* La fonction tokenize retourne un tableau de chaÃ®nes de caractÃ¨res
- * elle prend en paramÃ¨tre une chaine String (l'Ã©quation Ã  traiter)
- * Toutes les fonctions sont dÃ©clarÃ©es avec const dans un style fonctionnel
- * pour Ã©viter des effets de bord (side effects). 
- * On fera appel Ã  la composition de fonctions pour retourner les valeurs
+/* La fonction tokenize retourne un tableau de chaînes de caractères
+ * elle prend en paramètre une chaine String (l'équation à traiter)
+ * Toutes les fonctions sont déclarées avec const dans un style fonctionnel
+ * pour éviter des effets de bord (side effects). 
+ * On fera appel à la composition de fonctions pour retourner les valeurs
  */
 
 
 const tokenize= function (code) {
         var results = [];
         
-        //le regex permet d'isoler le paramÃ¨tre a: par exemple: 3x + 2 sera 
-        // dÃ©coupÃ©e en ['3x', '+', '2']
-        var tokenRegExp = /\s*([A-Za-z]+|[0-9]+x|\S)\s*/g;
+        //le regex permet d'isoler le paramètre a: par exemple: 3x + 2 sera 
+        // découpée en ['3x', '+', '2']
+        var tokenRegExp = /\s*(-?[0-9]*x|-?[\d]+|\S)\s*/g;
 
         var m;
         while ((m = tokenRegExp.exec(code)) !== null)
         results.push(m[1]);
         return results;
         
-};
+}
 
 /*
- * fonction qui retourne le paramÃ¨tre A de l'Ã©quation
+ * fonction qui retourne le paramètre A de l'équation
  * Param: code   type: String
  */
     const parametreA = function(code){
-    var tok = tokenize(code);
-	var paraA;
-	var pente= /[0-9]+x/;
-	var sansX= /[0-9]+/;
+    var tok = code;
+	var laPente=0;
+	var pente= /-?[0-9]+x/;
+	var sansX= /-?[0-9]+/;
 	var penteX= 'x';
+	var penteNegX="-x";
 	for (var i=0; i< tok.length; i++){
 		if(tok[i].match(pente)){
-			
-			// une fois la pente ax est trouvÃ©e, on veut retourner seulement l'entier a
+			// une fois la pente ax est trouvée, on veut retourner seulement l'entier a
 			// et supprimer le x
-			pente= tok[i].match(sansX);
-			paraA= pente;
-	
-			return paraA;
-		
-		// si la pente se prÃ©sente sous la forme x, c'est-Ã -dire sans coefficient
+			laPente+= Number(tok[i].match(sansX));
+		// si la pente se présente sous la forme x, c'est-à-dire sans coefficient
 		// visible, on remet 1 comme coeff
-		}else if (tok[i]==penteX){
-			pente= 1;
-			paraA= pente;
-			return paraA;
-			
+		}else if (tok[i]==penteX){			
+			laPente += 1;			
+		}else if(tok[i]==penteNegX){
+			laPente += Number(-1);
+		}
 	}
-	}
+	return laPente;
+	
 };
 
 /*
- * fonction qui retourne le paramÃ¨tre b d'une Ã©quation
+ * fonction qui retourne le paramètre b d'une équation
  * Param: code type: String
  */
 const parametreB = function(code){
-	var tok = tokenize(code);
-	var ordonnee;
+	var tok = code;
+	//alert(code);
+	var ordonnee=0;
 	for (var i=0; i< tok.length; i++){
 		
-		// puisque l'Ã©quation est divisÃ©e en 'ax', '+' ou '-', 'b'
-		// donc le seul Ã©lÃ©ment qui est un nombre est le paramÃ¨tre 'b'
+		// puisque l'équation est divisée en 'ax', '+' ou '-', 'b'
+		// donc le seul élément qui est un nombre est le paramètre 'b'
 		 if(!isNaN(tok[i])){
 			ordonnee= tok[i];
-			return ordonnee;
-	}
+			}
+	return ordonnee;
 	}
 		
 };    
+
 
 
 
