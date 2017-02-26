306b65eacbab6f4e78e74dfff193c30d7bdfcde9
MARCOMP\Marco <olmocinese@hotmail.com>
2017-02-26 12:29:02 -0500

    Les points p1 et p2 sont maintenant a' la bonne place sur la ligne/curve.

--- public_html/JS/indexScript.js
+++ public_html/JS/indexScript.js
@@ -18,18 +18,24 @@ function trace(){
     //alert("La pente = " + pente);
     var ordonnee= parametreB(tokenize(equation));
     //alert("Ordonnee = " + ordonnee);
-    var exp = exposant(tokenize(equation)); 
- 	//alert(exp);   
- 	var p = board.create('point', [1,ordonnee], {style:6, name:'p1'});
- 	var t = board.create('point', [pente,2], {style:6, name:'p2'});
- 	var k = board.create('point', [exp,3], {style:6, name:'p3'});
+    var exp = exposant(tokenize(equation));
+ 	alert(exp);
+
+if (exp == 0) {
+ 	var p = board.create('point', [1,(ordonnee+pente)], {style:6, name:'p1'});
+ 	var t = board.create('point', [(ordonnee/-pente), 0], {style:6, name:'p2'});
+} else {
+	var z = board.create('point', [1,(exp+ordonnee+pente)], {style:6, name:'p1'});
+	var y = board.create('point', [-1,exp+ordonnee+(-1*pente)], {style:6, name:'p2'});
+ 	var w = board.create('point', [exp,3], {style:6, name:'p3'});
+}
 	var sliderA =board.create('slider',[[4,-3],[6,-3],[pente-4,pente,pente+4]],{name:'a'});
 	var sliderB =board.create('slider',[[4,-3.5],[6,-3.5], [ordonnee -4, ordonnee,ordonnee +4]], {name:'b'});
 	var sliderC =board.create('slider',[[4,-4],[6,-4],[exp-4,exp,exp+4]],{name:'c'});
 	function f(x) {
         return sliderC.Value()*(x*x)+ sliderA.Value()*x + sliderB.Value();
-	
-	
+
+
 	}
         var stringEquation= board.create('text', [4,-2,function(){return 'y= '+sliderC.Value().toFixed(2)
 	+ 'x²' + (sliderA.Value()<0?'':'+')+ sliderA.Value().toFixed(2)
@@ -39,7 +45,7 @@ function trace(){
 
 
 
- 
+
 
 function clearAll(board){
 	JXG.JSXGraph.freeBoard(board);
@@ -51,7 +57,7 @@ function clearAll(board){
 /* La fonction tokenize retourne un tableau de cha�nes de caract�res
  * elle prend en param�tre une chaine String (l'�quation � traiter)
  * Toutes les fonctions sont d�clar�es avec const dans un style fonctionnel
- * pour �viter des effets de bord (side effects). 
+ * pour �viter des effets de bord (side effects).
  * On fera appel � la composition de fonctions pour retourner les valeurs
  */
 
@@ -59,14 +65,14 @@ function clearAll(board){
 const tokenize= function (code) {
         var results = [];
         var text = String.fromCharCode(178);
-        //le regex permet d'isoler le param�tre a: par exemple: 3x, + 2 sera 
+        //le regex permet d'isoler le param�tre a: par exemple: 3x, + 2 sera
         // d�coup�e en ['3x', '+', '2']
         var tokenRegExp  = new RegExp('\s*(-?[0-9]*x{1}'+text+'?|-?[0-9]+|\S)\s*','g');
         var m;
         while ((m = tokenRegExp.exec(code)) !== null)
         results.push(m[1]);
         return results;
-        
+
 };
 
 const exposant = function(code){
@@ -107,14 +113,14 @@ const exposant = function(code){
 			laPente+= Number(tok[i].match(sansX));
 		// si la pente se pr�sente sous la forme x, c'est-�-dire sans coefficient
 		// visible, on remet 1 comme coeff
-		}else if (tok[i]==penteX){			
-			laPente += 1;			
+		}else if (tok[i]==penteX){
+			laPente += 1;
 		}else if(tok[i]==penteNegX){
 			laPente += Number(-1);
 		}
 	}
 	return laPente;
-	
+
 };
 
 /*
@@ -125,21 +131,18 @@ const parametreB = function(code){
 	var tok = code;
 	var ordonnee=0;
 	for (var i=0; i< tok.length; i++){
-		
+
 		// puisque l'équation est divisée en 'ax', '+' ou '-', 'b'
 		// donc le seul élément qui est un nombre est le paramètre 'b'
 		 if(tok[i].match(/^-?[^x\+]+$/)){
 		 	//alert("La pente "+ tok[i].match(/^-?[^x\+]+$/));
 			ordonnee += Number(tok[i].match(/^-?[^x\+]+$/));
-			
+
 			}
-	
+
 	}
-	return ordonnee;	
-};     
-
-
-
+	return ordonnee;
+};
 
 
 function erase () {
