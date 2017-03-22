93ff45d01de31e3b284779781d2d28d5477cc56a
osaaoui <saaoui@gmail.com>
2017-03-22 00:03:42 -0400

    corriger le traçage de la couleur en remplaçant l'attribut strokeColor par color

--- public_html/JS/indexPoints.js
+++ public_html/JS/indexPoints.js
@@ -74,9 +74,9 @@ function animerPente(){
  */ 
  
 animerVariationEnY= function (){
-	    p1= board.create('point', [0, (ordonnee+pente)], {style:6, name:'a', trace:true,strokeColor: 'green'});
-		p2= board.create('point', [1, (ordonnee+pente)], {style:6, name:'b', trace:true,strokeColor: 'green'});
-		p3= board.create('point', [0, (ordonnee+0)], {style:6, name:'o', trace:true,strokeColor: 'green'});
+	    p1= board.create('point', [0, (ordonnee+pente)], {style:6, name:'a', trace:true,color: 'green'});
+		p2= board.create('point', [1, (ordonnee+pente)], {style:6, name:'b', trace:true,color: 'green'});
+		p3= board.create('point', [0, (ordonnee+0)], {style:6, name:'o', trace:true,color: 'green'});
 	return p3.moveTo([0,(ordonnee+pente)], 1500, {callback: animerVariationEnX}); 
 	
 };
@@ -90,9 +90,9 @@ animerVariationEnY= function (){
  
  
 animerVariationEnX= function(){
-	    p1= board.create('point', [0, (ordonnee+pente)], {style:6, name:'a', trace:true,strokeColor: 'green'});
-		p2= board.create('point', [1, (ordonnee+pente)], {style:6, name:'b', trace:true,strokeColor: 'green'});
-		p3= board.create('point', [0, (ordonnee+0)], {style:6, name:'o', trace:true,strokeColor: 'green'});
+	    p1= board.create('point', [0, (ordonnee+pente)], {style:6, name:'a', trace:true,color: 'green'});
+		p2= board.create('point', [1, (ordonnee+pente)], {style:6, name:'b', trace:true,color: 'green'});
+		p3= board.create('point', [0, (ordonnee+0)], {style:6, name:'o', trace:true,color: 'green'});
 
 	return p1.moveTo([1, (ordonnee+pente)], 1500);
 };
