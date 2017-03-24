7197f1ea0107554644e5a6bb31cd583b96622a08
dg691218 <dg691218@C32676.ens.uqam.ca>
2017-03-24 15:55:31 -0300

    ajutement dans les slider des parametre decimal sous la forme 0.0 ( un nombre(point) un  nombre)

--- public_html/JS/indexScript.js
+++ public_html/JS/indexScript.js
@@ -35,9 +35,9 @@ function trace(){
 
 // Rajouter l'option snapWidth:1 pour que la manipulation du slider ne retourne que des entiers (ie. pas de nombres fractionnaires, etc)
 function slides (pente, ordonnee, exp) {
-	sliderA =board.create('slider',[[4,-3],[6,-3],[pente-4,pente,pente+4]],{name:'&nbsp&nbsp&nbsp&nbspA', snapWidth:1});
-	sliderB =board.create('slider',[[4,-3.5],[6,-3.5], [ordonnee -4, ordonnee,ordonnee +4]], {name:'&nbsp&nbsp&nbsp&nbspB',snapWidth:1});
-	sliderC =board.create('slider',[[4,-4],[6,-4],[exp-4,exp,exp+4]],{name:'&nbsp&nbsp&nbsp&nbspC',snapWidth:1});
+	sliderA =board.create('slider',[[4,-3],[6,-3],[pente-4,pente,pente+4]],{name:'&nbsp&nbsp&nbsp&nbspA'});
+	sliderB =board.create('slider',[[4,-3.5],[6,-3.5], [ordonnee -4, ordonnee,ordonnee +4]], {name:'&nbsp&nbsp&nbsp&nbspB'});
+	sliderC =board.create('slider',[[4,-4],[6,-4],[exp-4,exp,exp+4]],{name:'&nbsp&nbsp&nbsp&nbspC'});
 	function f(x) {
 		return sliderC.Value()*(x*x)+ sliderA.Value()*x + sliderB.Value();
 	}
@@ -169,7 +169,7 @@ const tokenize= function (code) {
 	var text = String.fromCharCode(178);
 	//le regex permet d'isoler le paramètre a: par exemple: 3x, + 2 sera
 	// découpée en ['3x', '+', '2']
-	var tokenRegExp  = new RegExp('\s*(-?[0-9]*x{1}'+text+'?|-?[0-9]+|\S)\s*','g');
+	var tokenRegExp  = new RegExp('\s*(-?[0-9]*([.,][0-9]*)?x{1}'+text+'?|-?[0-9]+([.,][0-9]*)?|\S)\s*','g');
 	var m;
 	while ((m = tokenRegExp.exec(code)) !== null)
 	results.push(m[1]);
@@ -181,13 +181,15 @@ const exposant = function(code){
 	var tok=code;
 	var exp=0;
 	var text = String.fromCharCode(178);
-	var ouverture= new RegExp('-?[0-9]+x{1}'+text+'$');
-	var sansX= /-?[0-9]+/;
+	var ouverture= new RegExp('-?[0-9]+([.,][0-9]*)?x{1}'+text+'$');
+	var sansX= new RegExp('[^x'+text+']+');
 	var ouvertureX='x'+text;
 	var ouvertureNeg="-x"+text;
 	for(var i=0; i<tok.length;i++){
-		if(tok[i].match(ouverture)){
-			exp+= Number(tok[i].match(sansX));
+	
+		if(tok[i].match(ouverture)){;
+									
+			exp+= Number(tok[i].match(sansX)[0]);
 		}else if(tok[i]==ouvertureX){
 			exp++;
 		}else if(tok[i]==ouvertureNeg){
@@ -204,15 +206,15 @@ const exposant = function(code){
 const parametreA = function(code){
 	var tok = code;
 	var laPente=0;
-	var pente= /-?[0-9]+x$/;
-	var sansX= /-?[0-9]+/;
+	var pente= /-?[0-9]+([.,][0-9]*)?x$/;
+	var sansX= /[^x]+/;
 	var penteX= 'x';
 	var penteNegX="-x";
 	for (var i=0; i< tok.length; i++){
 		if(tok[i].match(pente)){
 			// une fois la pente ax est trouvée, on veut retourner seulement l'entier a
 			// et supprimer le x
-			laPente+= Number(tok[i].match(sansX));
+			laPente+= Number(tok[i].match(sansX)[0]);
 			// si la pente se présente sous la forme x, c'est-à-dire sans coefficient
 			// visible, on remet 1 comme coeff
 		}else if (tok[i]==penteX){
@@ -233,11 +235,10 @@ const parametreB = function(code){
 	var tok = code;
 	var ordonnee=0;
 	for (var i=0; i< tok.length; i++){
-
 		// puisque l'équation est divisée en 'ax', '+' ou '-', 'b'
 		// donc le seul élément qui est un nombre est le paramètre 'b'
 		if(tok[i].match(/^-?[^x\+]+$/)){
-			//alert("La pente "+ tok[i].match(/^-?[^x\+]+$/));
+			
 			ordonnee += Number(tok[i].match(/^-?[^x\+]+$/));
 
 		}
