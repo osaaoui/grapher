1916576fef23429d200621fc981f15ede246fbb2
MARCOMP\Marco <olmocinese@hotmail.com>
2017-04-08 16:12:20 -0400

    Le texte des les bulles dans la boite de text.

---
+++ public_html/JS/textpedagogique.js
@@ -0,0 +1,9 @@
+function textPente(){
+  document.getElementById('textPentePeda').style.display = 'block';
+  document.getElementById('textOrdoPeda').style.display = 'none';
+}
+
+function textOrd(){
+  document.getElementById('textOrdoPeda').style.display = 'block';
+    document.getElementById('textPentePeda').style.display = 'none'
+}
