24d68a9404eb6dfa21e2bec270c08bf38cdfe85e
MARCOMP\Marco <olmocinese@hotmail.com>
2017-04-10 19:36:51 -0400

    Resolution bug affichage

--- public_html/JS/textpedagogique.js
+++ public_html/JS/textpedagogique.js
@@ -1,20 +1,28 @@
+/*Functions pour cacher/montrer les text pedagogique*/
 function textPente(){
   document.getElementById('textPentePeda').style.display = 'block';
   document.getElementById('textOrdoPeda').style.display = 'none';
+  document.getElementById('textAxeSym').style.display = 'none';
+  document.getElementById('textLesZeros').style.display = 'none';
 }
 
 function textOrd(){
   document.getElementById('textOrdoPeda').style.display = 'block';
-    document.getElementById('textPentePeda').style.display = 'none';
-    
+  document.getElementById('textPentePeda').style.display = 'none';
+  document.getElementById('textAxeSym').style.display = 'none';
+  document.getElementById('textLesZeros').style.display = 'none';
 }
+
 function textAxe(){
 	document.getElementById('textAxeSym').style.display = 'block';
-    
+  document.getElementById('textPentePeda').style.display = 'none';
+  document.getElementById('textOrdoPeda').style.display = 'none';
+  document.getElementById('textLesZeros').style.display = 'none';
 }
 
 function textZeros(){
 	document.getElementById('textLesZeros').style.display = 'block';
 	document.getElementById('textAxeSym').style.display = 'none';
-    
+  document.getElementById('textPentePeda').style.display = 'none';
+  document.getElementById('textOrdoPeda').style.display = 'none';
 }
